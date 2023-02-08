86ebb88bb5fa0155cbdb845a445ec1d9a0e7c71d
upriyam-purdue <86703380+upriyam-purdue@users.noreply.github.com>
2023-02-08 16:38:41 -0500

    Update identify_card.py
    refactor & extract functionality into separate functions

--- identify_card.py
+++ identify_card.py
@@ -1,54 +1,24 @@
-# from io import BytesIO
-# from time import sleep
-# from picamera2.picamera2 import Picamera2
-from typing import Tuple, List, Set
-
+from typing import Tuple, List
 import numpy as np
-import skimage.measure
 import scipy.ndimage as img_filter
+import cv2
 
 
-def _bruteforce_match(img1: np.ndarray, img2: np.ndarray):
-    diff = np.abs(img1 - img2)
-    return diff, np.sum(diff * diff, axis=(0, 1))
+# noinspection PyShadowingNames
+def _load_image(path, scale=None):
+    img = cv2.imread(path)
+    if scale is not None:
+        img = cv2.resize(img, scale)
+    # returns [y, u, v] -- indexed in 3rd dimension [:,:, ??]
+    return cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
 
 
+# noinspection PyShadowingNames
 def _increase_contrast(img: np.ndarray, *, threshold=0.5):
     img = np.copy(img)
     img[img < threshold * 255] = 0
     img[img >= threshold * 255] = 255
     return img
-
-
-def _align_image(img: np.ndarray, *, height, width):
-    min_x, max_x, min_y, max_y = 0, len(img) - 1, 0, len(img[0]) - 1
-    while np.count_nonzero(img[min_x][:]) == 0:
-        min_x += 1
-    while np.count_nonzero(img[max_x][:]) == 0:
-        max_x -= 1
-    while np.count_nonzero(img[:][min_y]) == 0:
-        min_y += 1
-    while np.count_nonzero(img[:][max_y]) == 0:
-        max_y -= 1
-    # TODO rescale to height x width ??
-    return img[min_x:max_x][min_y:max_y]
-
-
-def _get_keypoints(img: np.ndarray):
-    # generate octaves
-    octaves = [img]
-    for _ in range(4):
-        octaves.append(skimage.measure.block_reduce(octaves[-1], (2, 2), np.max))  # max pooling to generate octaves
-    # extend octaves with gaussian blurs
-    img_table = []
-    for octave_img in octaves:
-        blurred = [octave_img]
-        for sig in range(5):
-            blurred.append(img_filter.gaussian_filter(octave_img, sigma=sig))
-        img_table.append(blurred)
-    # TODO compute differences of gaussian blurs for all octaves
-    # TODO identify keypoints as local extrema
-    # NTS probably don't need to eliminate keypoints since we want edge points
 
 
 # noinspection PyShadowingNames
@@ -61,11 +31,10 @@ def _get_edges(img: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.
 
 
 # noinspection PyShadowingNames
-def _filter_edges(img: np.ndarray, *, threshold=0.9) -> np.ndarray:
+def _filter_edges(img: np.ndarray) -> np.ndarray:
     x1, x2, y1, y2 = _get_edges(img)
-    cutoff = threshold * 255
     out = np.copy(img) * 0
-    out[np.logical_or(np.logical_or(x1 >= cutoff, x2 >= cutoff), np.logical_or(y1 >= cutoff, y2 >= cutoff))] = 255
+    out[np.logical_or(np.logical_or(x1 > 0, x2 > 0), np.logical_or(y1 > 0, y2 > 0))] = 255
     return out
 
 
@@ -85,6 +54,7 @@ def _get_bounding_boxes(img: np.ndarray) -> List[Tuple[int, int, int, int]]:
     def _get_parent(c):
         if parents[c] != c:
             parents[c] = _get_parent(parents[c])
+        # noinspection PyUnresolvedReferences
         return parents[c]
 
     for coord in coords:
@@ -127,59 +97,31 @@ def _get_bounding_boxes(img: np.ndarray) -> List[Tuple[int, int, int, int]]:
     return ret
 
 
-if __name__ == '__main__':
-    # img_width, img_height = 1920, 1440
-    # key_width, key_height = 0, 0  # height/width of top corner of card (with card rank/suit)
-    #
-    # cam = Picamera2()  # TODO configure camera for YUV mode
-    # cam.start()
-    # sleep(1)
-    # image = cam.capture_array()[:key_width, :key_height:, :3]  # extract 'top' nxm pixels of image
+# noinspection PyShadowingNames
+def preprocess_image(img: np.ndarray) -> Tuple[np.ndarray, List[Tuple[int, int, int, int]]]:
+    # img arg is dimensions h x w x 3 -- yuv for last dimension
+    y = img[:, :, 0]
+    y_contr = _increase_contrast(y, threshold=0.7)
+    edges = _filter_edges(y_contr)
 
-    import cv2
+    return edges, _get_bounding_boxes(edges)
 
 
-    def _load_image(path, scale=None):
-        img = cv2.imread(path)
-        if scale is not None:
-            img = cv2.resize(img, scale)
-        # returns y, u, v
-        return cv2.split(cv2.cvtColor(img, cv2.COLOR_BGR2YUV))
-
-
-    # y, u, v = _load_image('cards.jpeg')
-    y, u, v = _load_image('cards2.png')
-    # y, u, v = _load_image('/Users/up/Desktop/steins;gate.png')
-    img = _increase_contrast(y, threshold=0.7)
-    cv2.imshow('img', img)
-    cv2.waitKey(0)
-    # x1, x2, y1, y2 = _get_edges(y)
-    # x1 = _increase_contrast(x1, threshold=0.9)
-    # x2 = _increase_contrast(x2, threshold=0.9)
-    # y1 = _increase_contrast(y1, threshold=0.9)
-    # y2 = _increase_contrast(y2, threshold=0.9)
-    # img = np.copy(y) * 0
-    # img[np.logical_or(np.logical_or(x1 > 127, x2 > 127), np.logical_or(y1 > 127, y2 > 127))] = 255
-    img = _filter_edges(img, threshold=0.9)
-    cv2.imshow('img', img)
-    cv2.waitKey(0)
+# noinspection PyShadowingNames
+def _add_bounding_boxes_to_image(img: np.ndarray, bboxes: List[Tuple[int, int, int, int]]) -> np.ndarray:
     r, g, b = np.copy(img), np.copy(img), np.copy(img)
-
 
     def _bound(b1, b2):
         low = min(b1, b2)
         high = max(b1, b2)
         return range(low, high + 1)
 
-
     def _set_red(_r, _g, _b, _x, _y):
         _r[_x, _y] = 255
         _g[_x, _y] = 0
         _b[_x, _y] = 0
 
-
-    for x1, y1, x2, y2 in _get_bounding_boxes(img):
-        # print(f"({x1}, {y1}) to ({x2}, {y2})")
+    for x1, y1, x2, y2 in bboxes:
         for x in _bound(x1, x2):
             _set_red(r, g, b, x, y1)
             _set_red(r, g, b, x, y2)
@@ -187,25 +129,11 @@ if __name__ == '__main__':
             _set_red(r, g, b, x1, y)
             _set_red(r, g, b, x2, y)
 
-    img = cv2.merge((b, g, r))
-    cv2.imwrite('bounding_boxes.png', img)
-    # cv2.imshow('img', img)
-    # cv2.waitKey(0)
+    return cv2.merge((b, g, r))
 
-    # pic1 = _load_image('/Users/up/Desktop/abe_lincoln.jpeg', scale=(640, 800))[0]
-    # pic2 = _load_image('/Users/up/Desktop/PRIYAM_UTKARSH.jpeg', scale=(640, 800))[0]
-    #
-    # # cv2.imshow('pic1', pic1)
-    # # cv2.imshow('pic2', pic2)
-    # # cv2.waitKey(0)
-    #
-    # p1c = _increase_contrast(pic1)
-    # p2c = _increase_contrast(pic2)
-    #
-    # cv2.imshow('pic1', p1c)
-    # cv2.imshow('pic2', 255 - p2c)
-    # # cv2.waitKey(0)
-    #
-    # diff, score = _bruteforce_match(p1c, 255 - p2c)
-    # cv2.imshow(f"Score: {score}", diff)
-    # cv2.waitKey(0)
+
+if __name__ == '__main__':
+    img = _load_image('cards2.png')
+    img, bboxes = preprocess_image(img)
+    img = _add_bounding_boxes_to_image(img, bboxes)
+    cv2.imwrite('bounding_boxes.png', img)
